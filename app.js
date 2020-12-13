576fc13fe5c67f9a21eb72acd8ba2a80f04d05dd
HoussamMrabet <dodslim100@gmail.com>
2020-12-13 04:47:20 +0100

    adding sounds to the loop

--- app.js
+++ app.js
@@ -16,7 +16,27 @@ class DrumKit {
 
     repeat() {
         let step = this.index % 8;
-        const activeBars = document.querySelector(`.b${step}`);
+        const activeBars = document.querySelectorAll(`.b${step}`);
+        //Loop over the pads
+        activeBars.forEach(bar => {
+            bar.style.animation = `playTrack 0.3s alternate ease-in-out 2`;
+            //Check if pads are active
+            if (bar.classList.contains('active')) {
+                //Check each sound
+                if (bar.classList.contains('kick-pad')) {
+                    this.kickAudio.currentTime = 0;
+                    this.kickAudio.play();
+                }
+                if (bar.classList.contains('snare-pad')) {
+                    this.snareAudio.currentTime = 0;
+                    this.snareAudio.play();
+                }
+                if (bar.classList.contains('hihat-pad')) {
+                    this.hihatAudio.currentTime = 0;
+                    this.hihatAudio.play();
+                }
+            }
+        });
         this.index++;
     }
 
@@ -33,6 +53,9 @@ const drumKit = new DrumKit();
 
 drumKit.pads.forEach(pad => {
     pad.addEventListener('click', drumKit.activePad);
+    pad.addEventListener('animationend', function () {
+        this.style.animation = "";
+    })
 });
 
 drumKit.playBtn.addEventListener('click', function () {
